5cb951a2244b25d06c0c4eb5d10ac1e0a2dd5b0a
Ehab Feroze <163151060+ehab1593@users.noreply.github.com>
2024-11-07 16:54:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./library.js');
 
-// Write your code below:
-
 
 async function serveDinnerAgain(){
   let foodArray = await Promise.all([steamBroccoli(), cookRice(), bakeChicken(), cookBeans() ]);
